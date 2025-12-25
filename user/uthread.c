21523a1b49b7423e7b8d608efae66180906afcc4
Galaxy-Ivan <1011987704@qq.com>
2025-12-25 14:55:18 +0800

    Lab 7.1 Uthread: switching between threads (moderate) OK

--- user/uthread.c
+++ user/uthread.c
@@ -10,16 +10,22 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct context {
+  uint64 ra;
+  uint64 sp;
+  uint64 s[12];
+};
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
-
+  struct context context;
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
-extern void thread_switch(uint64, uint64);
-              
+// extern void thread_switch(uint64, uint64);
+extern void thread_switch(struct context *old, struct context *new);
+
 void 
 thread_init(void)
 {
@@ -63,6 +69,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch(&t->context, &current_thread->context);
   } else
     next_thread = 0;
 }
@@ -77,6 +84,8 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  t->context.ra = (uint64)func;
+  t->context.sp = (uint64)t->stack + STACK_SIZE; // 栈指针（栈末尾）
 }
 
 void 
